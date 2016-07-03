80cf9ffe73260be8f986202915a6bcd14bfcfebd
Chris Myers <cmyers78@gmail.com>
2016-07-03 11:30:22 -0600

    more updates....weird segue syntax.  don't understand it.

--- firstAppBook/firstAppBook/TimerListTableViewController.swift
+++ firstAppBook/firstAppBook/TimerListTableViewController.swift
@@ -17,6 +17,8 @@ class TimerListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
         coffeeTimers = [
             CoffeeTimerModel(name: "Colombian", duration: 240),
             CoffeeTimerModel(name: "Mexican", duration: 200)
@@ -74,50 +76,34 @@ class TimerListTableViewController: UITableViewController {
         }
     }
     
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        
+        if identifier == "pushDetail" {
+            if tableView.editing {
+                return false
+            }
+        }
+        
+        return true
+    }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "pushDetail" {
+            let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPathForCell(cell)!
+            
+            let timerModel : CoffeeTimerModel = {
+                if indexPath.section == 0 {
+                    return self.coffeeTimers[indexPath.row]
+                } else {
+                    return self.teaTimers[indexPath.row]
+                }
+            } ()
+            
+            
+            let detailViewController = segue.destinationViewController as! TimerDetailViewController
+            detailViewController.timerModel = timerModel
+            
+        }
     }
-    */
-
 }
